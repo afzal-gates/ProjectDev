15793338a13322bb7296a853fe068a4f8aea02ac
ProjectDev <afzal.gates@gmail.com>
2020-07-25 12:34:32 +0600

    Photo

--- ProjectDev/ClientApp/src/app/pages/forms/employer-form/employer-form.component.ts
+++ ProjectDev/ClientApp/src/app/pages/forms/employer-form/employer-form.component.ts
@@ -8,4 +8,18 @@ import { Component, HostBinding } from '@angular/core';
 export class EmployerFormComponent {
   @HostBinding('class.employer-form') private readonly employerForm = true;
   public qualifications = ['Young Padawan', 'Junior', 'Middle', 'Senior'];
+
+  imageSrc: string;
+
+  onFileChange(event) {
+
+    const reader = new FileReader();
+    if (event.target.files && event.target.files.length) {
+      const [file] = event.target.files;
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.imageSrc = reader.result as string;
+      };
+    }
+  }
 }
